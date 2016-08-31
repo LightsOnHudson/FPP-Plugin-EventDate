7212d4632a121734e5fa816aacba42278819f027
LightsOnHudson <brbshaver@gmail.com>
2016-08-31 13:47:54 -0600

    install script

--- fpp_install.sh
+++ fpp_install.sh
@@ -2,5 +2,5 @@
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
 exec /opt/fpp/scripts/update_plugin ${target_PWD##*/}
-/bin/cp RUN-COUNTDOWN-SCRIPT.php ../../scripts/
+/bin/cp /home/fpp/media/plugins/EventDate/RUN-COUNTDOWN-SCRIPT.php /home/fpp/media/scripts
 popd
